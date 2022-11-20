d5f739a1fb82beb604e35c4d60e38c530ec8c243
WisniewskiGabriel <gabwisniewski@gmail.com>
2022-11-20 16:23:18 -0300

    ajuste na msg do ex 5

--- Ex. 5/index.js
+++ Ex. 5/index.js
@@ -1,8 +1,8 @@
 let valorDigitado = prompt("Digite um valor");
-let strMsg = "Não é maior que 100";
+let strMsg = "NÃO É MAIOR QUE 100!";
 
 if(valorDigitado>100){
-    strMsg="Maior que 100";
+    strMsg="É MAIOR QUE 100!";
 }
 if(isNaN(valorDigitado)){
     strMsg = "Não é um número";
